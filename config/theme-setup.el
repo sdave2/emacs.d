5263de3b99c09cdd54b607c3dd25e24f559d2c74
Shwetank Dave <s.dave.p@gmail.com>
2014-04-28 10:41:31 -0400

    Changing emacs theme based on time of day.

--- config/theme-setup.el
+++ config/theme-setup.el
@@ -1,6 +1,22 @@
 (require-package 'color-theme-solarized)
+(setq current-theme '(color-theme-solarized-light))
+(defun set-theme ()
+  (progn
+    (setq hour
+          (string-to-number
+           (substring (current-time-string) 11 13)))
+    (if (member hour (number-sequence 6 18))
+        (setq now '(color-theme-solarized-dark))
+      (setq now '(color-theme-solarized-light))))
+  (if (eq now current-theme)
+      nil
+    (setq current-theme now))
+  (eval current-theme))
 
 (after 'color-theme-solarized-autoloads
   (color-theme-initialize)
-  (load-theme 'solarized-light t)
+  (set-theme)
   (setq color-theme-is-global t))
+
+;; Set a timer to change them after 6:00 pm.
+(run-at-time 0 3600 'set-theme)
