e0ab9a90f4de654dbf2ca8d1f05dfb39ff33be9c
Shwetank Dave <shwetank.dave@cenx.com>
2015-07-02 12:14:06 -0400

    Adding 'copy-buffer-name'

--- shwetankdave/key-bindings.el
+++ shwetankdave/key-bindings.el
@@ -3,6 +3,12 @@
   (interactive)
   (message "Copied filename %s" (kill-new (buffer-file-name))))
 
+(defun copy-buffer-name ()
+  (interactive)
+  (message "Copied buffer name %s" (kill-new (buffer-name))))
+
 ;; -- all keybindings --
 (bind-key "C-c c" 'copy-filename)
+(bind-key "C-c b" 'copy-buffer-name)
 (bind-key "M-t" 'toggle-window-split)
+(bind-key "M-~" 'other-frame)
